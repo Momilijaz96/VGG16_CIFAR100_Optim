f7ba89c338f7847ffc901566dae1fa86d4bc095b
Momil Ijaz <momilijaz@gmail.com>
2021-08-31 18:33:35 -0400

    Update VGG16.py

--- VGG16.py
+++ VGG16.py
@@ -1,4 +1,21 @@
+import torch.nn as nn
 import torchvision.models as models
-vgg16 = models.vgg16()
+
+
+model = models.vgg16(pretrained=True) #130million+ parameters
+
+#Freeze all model parameters
+for param in model.parameters():
+    param.requires_grad = False
+
+
+#Add on classifier
+# Add on classifier
+n_classes=100
+n_inputs=4096
+model.classifier[6] = nn.Sequential(
+                      nn.Linear(n_inputs, n_classes),                 
+                      nn.LogSoftmax(dim=1))
+
 
 
